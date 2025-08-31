2fef0c81b4be930b02b7b13808f6cacf35f0ef81
Vinay Singh Patel <vinaypatel898944@gmail.com>
2025-08-31 14:06:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 5000;
-const databaseConnect =  require('./ConnectToDatabase');
+const databaseConnect = require('./ConnectToDatabase');
 const NotesRouter = require('./controllers/Notes-Controller');
 const UserRouter = require('./controllers/user-controller');
 const { router } = require('./controllers/EmailOtpVerify');
 const cors = require('cors');
 
+// Use Render's PORT (or fallback to 5000 locally)
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    console.log("Server Started");
-})
+app.get('/', (req, res) => {
+  console.log("Server Started");
+  res.send("Backend is running "); 
+});
 
 const allowedOrigins = [
   'http://localhost:3000',
+  'https://effervescent-donut-c3775b.netlify.app/',
+  'https://effervescent-donut-c3775b.netlify.app',
 ];
 
 app.use(cors({
@@ -29,13 +33,12 @@ app.use(cors({
   credentials: true 
 }));
 
-
 databaseConnect();
 
-app.use('/api/User',UserRouter);
-app.use('/Notes',NotesRouter);
-app.use('/otp',router);
+app.use('/api/User', UserRouter);
+app.use('/Notes', NotesRouter);
+app.use('/otp', router);
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(` Server is running on port ${PORT}`);
+});
